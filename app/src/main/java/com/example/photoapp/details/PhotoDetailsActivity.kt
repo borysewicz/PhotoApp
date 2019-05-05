663bb3e050849769a009ff8deae4d33767d2fe84
borysewicz <33198052+borysewicz@users.noreply.github.com>
2019-05-05 13:12:28 +0200

    Change PhotoDetailsActivity layout to TabLayout. Add titles to tabs.

--- app/src/main/java/com/example/photoapp/details/PhotoDetailsActivity.kt
+++ app/src/main/java/com/example/photoapp/details/PhotoDetailsActivity.kt
@@ -2,11 +2,13 @@ package com.example.photoapp.details
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
+import android.widget.TableLayout
 import com.example.photoapp.R
 import com.example.photoapp.model.PhotoModel
 import com.example.photoapp.recyclerview.PhotoAdapter.Companion.MODEL_KEY
@@ -14,15 +16,22 @@ import com.example.photoapp.recyclerview.PhotoAdapter.Companion.MODEL_KEY
 
 class PhotoDetailsActivity : FragmentActivity() {
 
-    private val PAGES = 1
+    companion object {
+        private val PAGES = 1
+        private val PHOTO_FULL = "Full photo"
+    }
     private lateinit var pager: ViewPager
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
         pager = findViewById(R.id.photo_details_VP)
         pager.adapter = ScreenSlidePagerAdapter(supportFragmentManager)
-     //   addPhotoFullFragment()
+
+        val tabLayout = findViewById<TabLayout>(R.id.photo_details_TL)
+        tabLayout.setupWithViewPager(pager)
     }
 
 
@@ -31,6 +40,13 @@ class PhotoDetailsActivity : FragmentActivity() {
 
         override fun getItem(position: Int): Fragment =
             PhotoFullFragment.newInstance((intent.getSerializableExtra(MODEL_KEY) as PhotoModel).url)
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            when(position){
+                0 ->  return PHOTO_FULL
+            }
+            return "Error"
+        }
     }
 
 }
